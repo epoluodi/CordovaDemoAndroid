90ed1fcfc368dac31356538b691394d7fcabdd24
Apollo Yang <epoluodi@gmail.com>
2016-12-01 23:45:35 +0800

    创建

---
+++ app/src/main/java/com/example/yangxiaoguang/cordovademo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.yangxiaoguang.cordovademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.apache.cordova.CordovaWebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
